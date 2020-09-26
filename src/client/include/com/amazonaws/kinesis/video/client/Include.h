ccab0f12bb21937edb63d51cd72212f369020e21
ycyang <yc.yang1229@gmail.com>
2020-09-26 15:22:43 +0800

    correct the parameters of version and arn.

--- src/client/include/com/amazonaws/kinesis/video/client/Include.h
+++ src/client/include/com/amazonaws/kinesis/video/client/Include.h
@@ -234,13 +234,15 @@ extern "C" {
 
 /**
  * Max update version length in chars
+ * https://docs.aws.amazon.com/zh_tw/kinesisvideostreams/latest/dg/API_DeleteSignalingChannel.html#KinesisVideo-DeleteSignalingChannel-request-CurrentVersion
  */
-#define MAX_UPDATE_VERSION_LEN                   128
+#define MAX_UPDATE_VERSION_LEN                   64
 
 /**
  * Max ARN len in chars
+ * https://docs.aws.amazon.com/zh_tw/kinesisvideostreams/latest/dg/API_DescribeSignalingChannel.html#API_DescribeSignalingChannel_RequestSyntax
  */
-#define MAX_ARN_LEN                              2048
+#define MAX_ARN_LEN                              1024
 
 /**
  * Max len of the auth data (STS or Cert) in bytes
