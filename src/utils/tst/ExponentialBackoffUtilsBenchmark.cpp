dcf6767dc082781bedf49201741fd93c6d03ed02
Anurag Katey <akatey@amazon.com>
2021-11-12 10:01:03 -0800

    Improved expo retry with benchmark

--- src/utils/tst/ExponentialBackoffUtilsBenchmark.cpp
+++ src/utils/tst/ExponentialBackoffUtilsBenchmark.cpp
@@ -5,93 +5,136 @@
 #include <fstream>
 #include <iterator>
 
-class ExponentialBackoffUtilsBenchmark : public UtilTestBase {
+class Server {
 private:
-    std::atomic<int> serverCounter;
+    std::atomic<int> requestCounter;
     std::vector<long> requestArrivalTimeStamps;
     std::mutex serverLock;
-    long currentTime;
+    long baseTime;
 
-    void makeAPICall() {
-        serverCounter++;
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+    std::string metricsFile;
+    int metricsPeriodMilliseconds;
+
+public:
+    Server() {}
+    explicit Server(const std::string& metricsFile, int metricsPeriodMilliseconds) {
+        std::atomic_init(&requestCounter, 0);
+        this->baseTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now().time_since_epoch()).count();
+        this->metricsFile = metricsFile;
+        this->metricsPeriodMilliseconds = metricsPeriodMilliseconds;
+    }
+
+    ~Server() {
+        std::cout << "\n Total requests received on server: " << requestCounter;
+        flushRequestTimestampsToFile();
+    }
+
+    void fakeAPI() {
+        requestCounter++;
+        auto requestArrivalTime = std::chrono::duration_cast<std::chrono::milliseconds>(
                 std::chrono::system_clock::now().time_since_epoch());
 
         serverLock.lock();
-        requestArrivalTimeStamps.emplace_back((ms.count() - currentTime));
+        requestArrivalTimeStamps.emplace_back((requestArrivalTime.count() - baseTime));
         serverLock.unlock();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(RAND() % 10));
     }
 
-    void threadTask() {
-        PExponentialBackoffState pExponentialBackoffState = NULL;
-        UINT32 maxTestRetryCount = 10;
+private:
+    void flushRequestTimestampsToFile() {
+        std::cout << "\n requestArrivalTimeStamps: " << requestArrivalTimeStamps.size();
+        if (requestArrivalTimeStamps.empty()) {
+            return;
+        }
 
+        std::map<long, int> requestsMap;
+        long largestTimeStamp = requestArrivalTimeStamps.back();
+        for (int i = 0; i < largestTimeStamp; i += metricsPeriodMilliseconds) {
+            requestsMap[i] = 0;
+        }
+
+        long startTimeStamp = 0;
+        for (auto& timestamp : requestArrivalTimeStamps) {
+            if (timestamp <= startTimeStamp + metricsPeriodMilliseconds) {
+                requestsMap[startTimeStamp + metricsPeriodMilliseconds]++;
+            } else {
+                startTimeStamp += metricsPeriodMilliseconds;
+            }
+        }
+
+        // trim leading and trailing 0 entries
+        auto startIter = requestsMap.begin();
+        for (; startIter->second != 0 && startIter != requestsMap.end(); startIter++);
+
+        if (startIter == requestsMap.end()) {
+            std::cout << "\n No metrics found on server";
+            return;
+        }
+
+        auto endIter = --requestsMap.end();
+        for (; endIter->second != 0 && endIter != requestsMap.begin(); endIter--);
+
+        std::ofstream file;
+        file.open(metricsFile);
+        std::cout << "\n Writing server metrics to file: " << metricsFile << "   requestsMap: " << requestsMap.size();
+
+        while(startIter <= endIter) {
+            file << startIter->first << "\t" << startIter->second << "\n";
+            startIter++;
+        }
+
+        file.close();
+    }
+};
+
+class ExponentialBackoffUtilsBenchmark : public UtilTestBase {
+    void threadTask(Server* server, int maxRetryCount, ExponentialBackoffJitterType jitterType) {
+        PExponentialBackoffState pExponentialBackoffState = NULL;
         EXPECT_EQ(STATUS_SUCCESS, exponentialBackoffStateWithDefaultConfigCreate(&pExponentialBackoffState));
-        pExponentialBackoffState->exponentialBackoffConfig.jitterType = FIXED_JITTER;
+        pExponentialBackoffState->exponentialBackoffConfig.jitterType = jitterType;
 
         EXPECT_TRUE(pExponentialBackoffState != NULL);
 
-        for (int i = 1; i <= maxTestRetryCount; i++) {
+        for (int i = 1; i <= maxRetryCount; i++) {
             auto status = exponentialBackoffBlockingWait(pExponentialBackoffState);
-            makeAPICall();
-            if (status != STATUS_SUCCESS) {
-            }
+            server->fakeAPI();
         }
 
         EXPECT_EQ(STATUS_SUCCESS, exponentialBackoffStateFree(&pExponentialBackoffState));
     }
 
-    std::map<long, int> processRequestArrivalTimestamps(std::vector<long>& requestArrivalTimestamps) {
-        std::map<long, int> bucketRequests;
-        for (int i = 1000; i < 30000; i+=100) {
-            bucketRequests[i] = 0;
-        }
-        long startTimeStamp = 1000;
-
-        for (auto& timestamp : requestArrivalTimestamps) {
-            if (timestamp <= startTimeStamp + 100) {
-                bucketRequests[startTimeStamp + 100]++;
-            } else {
-                startTimeStamp += 100;
-            }
-        }
-        return bucketRequests;
-    }
-
-    void dumpToFile(std::map<long, int>& groupedRequests) {
-        std::ofstream file;
-        file.open("/tmp/exponential_backoff_benchmark_details.txt");
-        for (auto it = groupedRequests.begin(); it != groupedRequests.end(); it++) {
-            file << it->first << "\t" << it->second << "\n";
-        }
-        file.close();
-    }
-
 public:
-    void startBenchMarkTest() {
+
+    void startBenchMarkTest(
+            int concurrentThreads,
+            int maxRetryCount,
+            int metricsPeriodMilliseconds,
+            const std::string& testResultsFilePath,
+            ExponentialBackoffJitterType jitterType) {
+
         srand(time(0));
-        std::atomic_init(&serverCounter, 0);
-        currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(
-                std::chrono::system_clock::now().time_since_epoch()).count();
+
         std::vector<std::thread> threadPool;
-        for (int i = 1; i <= 4000; i++) {
-            threadPool.emplace_back(std::thread(&ExponentialBackoffUtilsBenchmark::threadTask, this));
+        Server server(testResultsFilePath, metricsPeriodMilliseconds);
+
+        for (int i = 1; i <= concurrentThreads; i++) {
+            threadPool.emplace_back(std::thread(&ExponentialBackoffUtilsBenchmark::threadTask, this, &server, maxRetryCount, jitterType));
         }
 
         for (auto& th : threadPool) {
+            std::cout << "\n Joining thread";
             th.join();
         }
-
-        std::map<long, int> groupedRequests = processRequestArrivalTimestamps(requestArrivalTimeStamps);
-        dumpToFile(requestArrivalTimeStamps);
-        dumpToFile(groupedRequests);
-        std::cout << "\n Total server calls: " << serverCounter;
     }
 };
 
 TEST_F(ExponentialBackoffUtilsBenchmark, exponential_backoff_utils_benchmark_test)
 {
-    startBenchMarkTest();
+    int concurrentThreads = 20;
+    int maxRetryCount = 3;
+    int metricsPeriodMilliseconds = 10;
+    std::string testResultsFilePath = "/tmp/exponential_backoff_benchmark_details_no_jitter.txt";
+    startBenchMarkTest(concurrentThreads, maxRetryCount, metricsPeriodMilliseconds, testResultsFilePath, NO_JITTER);
 }
