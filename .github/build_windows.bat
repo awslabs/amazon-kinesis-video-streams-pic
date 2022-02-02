4436755cab8e53621029e6e9eb9f423c3df97072
Niyati Maheshwari <niyatim23@gmail.com>
2022-02-02 14:53:11 -0800

    fix test instruction

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 mkdir build
 cd build
 cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE ..
