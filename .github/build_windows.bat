a0fac57ba8c0958ea8c56fd099e3e513c9f46fce
Niyati Maheshwari <niyatim23@gmail.com>
2022-02-04 14:23:32 -0800

    disable windows defender

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x_86
 mkdir build
 cd build
 cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE ..
