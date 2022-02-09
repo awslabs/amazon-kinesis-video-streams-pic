d888f39ba8c05a3a8003a3c446b1751d34a9a2b0
Niyati Maheshwari <niyatim23@gmail.com>
2022-02-09 10:29:06 -0800

    fix msvc path

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
 cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE ..
