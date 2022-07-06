133cbd7a356f3d07dafa17548bd08b02bc9decfd
Niyati Maheshwari <niyatim23@gmail.com>
2022-07-06 14:16:42 -0700

    update putStreamCurl timestamp function

--- src/client/src/StreamState.c
+++ src/client/src/StreamState.c
@@ -813,7 +813,7 @@ STATUS executePutStreamState(UINT64 customData, UINT64 time)
         // Call API
         CHK_STATUS(pKinesisVideoClient->clientCallbacks.putStreamFn(
             pKinesisVideoClient->clientCallbacks.customData, pKinesisVideoStream->streamInfo.name, (PCHAR) MKV_CONTAINER_TYPE_STRING,
-            pKinesisVideoClient->clientCallbacks.getCurrentTimeFn(pKinesisVideoClient->clientCallbacks.customData),
+            GETTIME(),
             pKinesisVideoStream->streamInfo.streamCaps.absoluteFragmentTimes, pKinesisVideoStream->streamInfo.streamCaps.fragmentAcks,
             pKinesisVideoStream->streamingEndpoint, &pKinesisVideoStream->base.serviceCallContext));
 
