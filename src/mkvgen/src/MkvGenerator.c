2897f91c3feb882afbc7868bb4dadf6ba50c3ce4
James Delaplane <delaplan@amazon.com>
2023-04-24 10:03:51 -0700

    Debugging

--- src/mkvgen/src/MkvGenerator.c
+++ src/mkvgen/src/MkvGenerator.c
@@ -1578,7 +1578,7 @@ STATUS mkvgenEbmlEncodeSimpleBlock(PBYTE pBuffer, UINT32 bufferSize, INT16 times
                 MEMCPY(pCurrent, pE2EE->iv, EVP_MAX_IV_LENGTH);
                 pCurrent += EVP_MAX_IV_LENGTH;
             }
-            CHK_STATUS(aesEncrypt(pFrame->frameData, (INT64)adaptedFrameSize, pE2EE->key, pE2EE->iv, pCurrent, &encryptedFrameSize));
+            CHK_STATUS(aesEncrypt(pFrame->frameData, pFrame->size, pE2EE->key, pE2EE->iv, pCurrent, &encryptedFrameSize));
             adaptedFrameSize = encryptedFrameSize;
             if(CHECK_FRAME_FLAG_KEY_FRAME(pFrame->flags)) {
                 adaptedFrameSize += EVP_MAX_IV_LENGTH + EVP_MAX_KEY_LENGTH;
