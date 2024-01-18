1b19887f4c2e3d7a68bfacf8605796cb5be02b04
Divya Sampath Kumar <disa6302@colorado.edu>
2024-01-18 14:00:03 -0800

    Clang formatted

--- src/utils/src/CustomAssert.c
+++ src/utils/src/CustomAssert.c
@@ -1,9 +1,9 @@
 #include "Include_i.h"
 
-VOID customAssert(INT32 condition, const CHAR* fileName, INT32 lineNumber, const CHAR* functionName) {
-    printf("Using customAssert\n");
+VOID customAssert(INT32 condition, const CHAR* fileName, INT32 lineNumber, const CHAR* functionName)
+{
     if (!condition) {
         fprintf(stderr, "Assertion failed in file %s, function %s, line %d \n", fileName, functionName, lineNumber);
-        abort();
+        abort(); // C function call
     }
 }
