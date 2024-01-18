91c55320efa8c3867a2b8bd919ccf3d9056a10eb
Divya Sampath Kumar <disa6302@colorado.edu>
2024-01-18 12:40:02 -0800

    Fix fprintf

--- src/utils/src/CustomAssert.c
+++ src/utils/src/CustomAssert.c
@@ -3,7 +3,13 @@
 VOID customAssert(INT32 condition, const CHAR* fileName, INT32 lineNumber, const CHAR* functionName, const CHAR* format, ...) {
     printf("Using customAssert\n");
     if (!condition) {
-        fprintf(stderr, "Assertion failed in %s (%s:%d)\nFunction: %s\nMessage: ", fileName, functionName, lineNumber);
+        fprintf(stderr, "Assertion failed in file %s, function %s, line %d. Message: ", fileName, functionName, lineNumber);
+        va_list args;
+        va_start(args, format);
+        vfprintf(stderr, format, args);
+        va_end(args);
+
+        fprintf(stderr, "\n");
         abort();
     }
 }
