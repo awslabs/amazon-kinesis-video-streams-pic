b36d7381cca5cd141d9b13735c7d5d5d05e04e9c
Divya Sampath Kumar <disa6302@colorado.edu>
2024-01-19 15:52:39 -0800

    have persisted ack check

--- src/client/src/Stream.c
+++ src/client/src/Stream.c
@@ -1283,11 +1283,18 @@ STATUS getStreamData(PKinesisVideoStream pKinesisVideoStream, UPLOAD_HANDLE uplo
             if (pKinesisVideoStream->streamStopped) {
                 // Get the duration and the size
                 CHK_STATUS(getAvailableViewSize(pKinesisVideoStream, &duration, &viewByteSize));
-                DLOGW("[%s] Indicating a stop stream request in streaming state %", PRIu64, pKinesisVideoStream->streamInfo.name, uploadHandle);
+                DLOGW("[%s] Indicating a stop stream request in streaming state", pKinesisVideoStream->streamInfo.name);
                 if (viewByteSize == 0) {
-                    DLOGI("[%s] Buffer empty and stream stop request received, moving to TERMINATED state for %", PRIu64,
-                          pKinesisVideoStream->streamInfo.name, uploadHandle);
-                    pUploadHandleInfo->state = UPLOAD_HANDLE_STATE_TERMINATED;
+                    DLOGI("[%s] Buffer empty and stream stop request received",
+                          pKinesisVideoStream->streamInfo.name);
+                    if (pUploadHandleInfo->lastPersistedAckTs == pUploadHandleInfo->lastFragmentTs) {
+                        DLOGI("[%s] Last ack received, moving to TERMINATED state ", pKinesisVideoStream->streamInfo.name);
+                        pUploadHandleInfo->state = UPLOAD_HANDLE_STATE_TERMINATED;
+                        CHK(FALSE, STATUS_END_OF_STREAM);
+                    } else {
+                        DLOGI("[%s] Stay in streaming state till we receive last ack", pKinesisVideoStream->streamInfo.name);
+                        pUploadHandleInfo->state = UPLOAD_HANDLE_STATE_STREAMING;
+                    }
                 }
             }
             break;
