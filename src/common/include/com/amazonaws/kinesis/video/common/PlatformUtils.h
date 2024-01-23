8216b9830fbb4291ed9955b1b0184329109b38d7
Divya Sampath Kumar <disa6302@colorado.edu>
2024-01-23 17:22:22 -0500

    Assert handle (#239)
    * Use abort instead
    
    * custom assert
    
    * Fix definition
    
    * Fix fprintf
    
    * Simple assert
    
    * Clang formatted
    
    * Fix build issue

--- src/common/include/com/amazonaws/kinesis/video/common/PlatformUtils.h
+++ src/common/include/com/amazonaws/kinesis/video/common/PlatformUtils.h
@@ -35,14 +35,21 @@ extern logPrintFunc globalCustomLogPrintFn;
 // Compiling with NDK
 #include <android/log.h>
 #define __LOG(p1, p2, p3, ...)    __android_log_print(p1, p2, p3, ##__VA_ARGS__)
-#define __ASSERT(p1, p2, p3, ...) __android_log_assert(p1, p2, p3, ##__VA_ARGS__)
+#define __ASSERT(p1, p2, p3, ...) __android_log_assert(p1, p2, p3, ##__VA_ARGS__) // This is unaffected by release vs debug build unlike C assert().
 #else
 // Compiling under non-NDK
 #include <stddef.h>
 #include <stdlib.h>
 #include <assert.h>
+
+#ifdef DEBUG_BUILD
 #define __ASSERT(p1, p2, p3, ...) assert(p1)
-#define __LOG                     globalCustomLogPrintFn
+#else
+PUBLIC_API VOID customAssert(INT64 condition, const CHAR* fileName, INT64 lineNumber, const CHAR* functionName);
+#define __ASSERT(p1, p2, p3, ...) customAssert((p1), __FILE__, __LINE__, __FUNCTION__)
+#endif
+
+#define __LOG globalCustomLogPrintFn
 #endif // ANDROID_BUILD
 
 #define LOG_LEVEL_VERBOSE 1
