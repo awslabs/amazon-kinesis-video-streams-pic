88de716cf07c2ea3ca9f676e384baa5541b780c6
Divya Sampath Kumar <disa6302@colorado.edu>
2024-02-20 16:48:50 -0800

    Hash bucket index

--- src/utils/src/HashTable.c
+++ src/utils/src/HashTable.c
@@ -270,7 +270,7 @@ STATUS hashTableUpsert(PHashTable pHashTable, UINT64 key, UINT64 value)
     pHashBucket = getHashBucket(pHashTable, key);
     CHK(pHashBucket != NULL, STATUS_INTERNAL_ERROR);
     pHashEntry = pHashBucket->entries;
-    DLOGI("[%s] Hash table count: %d, %d", pHashTable->name, pHashBucket->count,  pHashBucket->length);
+    DLOGI("[%s] Hash table count: %d:%d, %d", pHashTable->name, key, pHashBucket->count,  pHashBucket->length);
     // Check if we already have the value
     for (i = 0; i < pHashBucket->count; i++, pHashEntry++) {
         if (pHashEntry->key == key) {
